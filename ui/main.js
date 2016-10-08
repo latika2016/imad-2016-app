433d7fe0a8fd03e76e0d59e1a942fd027bd1e9ab
latika2016 <latika.mailbox@gmail.com>
2016-10-08 12:29:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-console.log('Loaded!');
+// console.log('Loaded!');
+
+// Counter Code
+
+var button = document.getElementById("counter");
+var counter = 0;
+
+button.onclick = function (){
+    // Create a request
+    
+    // Capture the response and store it in a variable
+    
+    // Make a request
+    counter = counter + 1;
+    document.getElementById('count').value = counter;
+};
+
